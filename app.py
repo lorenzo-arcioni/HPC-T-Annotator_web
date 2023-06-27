1f9995a5d255626cc961a1ab649e3f305945555e
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2023-06-28 00:50:04 +0200

    geter

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, send_file, render_template
+from creator import *
 
 app = Flask(__name__)
 
@@ -6,9 +7,31 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         wlm = request.form['workload']
-        contenuto = f"Il nome inserito è: {wlm}"
-        with open('file.txt', 'w') as f:
-            f.write(contenuto)
+
+        if wlm == 'Slurm':
+            account_name = request.form['Slurm_account_name']
+            serial_part = request.form['Slurm_serial_part']
+            parallel_part = request.form['Slurm_parallel_part']
+            Nproces = request.form['Slurm_Nproces']
+            Nthread = request.form['Slurm_Nthread']
+            Time = request.form['Time']
+            Mproces = request.form['Mproces']
+
+        elif wlm == 'HTCondor':
+            pass
+        else:
+            pass
+
+        diamond = 1 if request.form['annotation_software'] == 'Diamond' else 0
+        tool = request.form['tool']
+        database = request.form['DB_path']
+        inputfile = request.form['IF_path']
+        outformat = request.form['outformat']
+        binary = request.form['BI_path']
+
+        fill_readbase(processes, threads, inputfile, outformat, diamond, tool, binary, database)
+        
+
         return send_file('file.txt', as_attachment=True)
 
     return open("index.html", "r").read()
