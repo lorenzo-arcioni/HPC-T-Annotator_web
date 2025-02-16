0f58fcd4a18a549dcce1805b21f35570e5f32db1
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2025-02-16 13:50:53 +0100

    vai

--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+mkdir -p log/nginx
+touch log/nginx/access.log
+touch log/nginx/error.log
+
+mkdir -p log/uwsgi
+touch log/uwsgi/uwsgi_access.log
+touch log/uwsgi/uwsgi_error.log
+
+./clear_log_files.sh
+
 docker compose up --build -d
